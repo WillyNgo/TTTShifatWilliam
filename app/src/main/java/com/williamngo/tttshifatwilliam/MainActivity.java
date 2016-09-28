09ef57f66a4be1aae75da68680e4865e960483b9
Willy <Willy>
2016-09-27 21:40:25 -0400

    coded some endGame(), increments number for scores

--- app/src/main/java/com/williamngo/tttshifatwilliam/MainActivity.java
+++ app/src/main/java/com/williamngo/tttshifatwilliam/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import static com.williamngo.tttshifatwilliam.R.string.vsDroid;
+
 public class MainActivity extends AppCompatActivity {
     public boolean againstDroid = true; //Determines whether user is playing against the droid or another human
     public boolean playersTurn = true; //Determines whose turn it currently is
@@ -176,12 +178,69 @@ public class MainActivity extends AppCompatActivity {
             imgBtn.setOnClickListener(null);
 
         //Increment wins
-        if(player == 0)
-            ;
-        if(player == 1)
-            ;
+        if(player == 0) // If user wins
+        {
+            //Get textview of player 1's wins and increment
+            TextView p1wins_textview = (TextView)findViewById(R.id.p1_wins);
+            int p1wins = Integer.parseInt(p1wins_textview.getText().toString());
+            p1wins++;
+            p1wins_textview.setText(p1wins);
+
+            //Increment losses of player 2
+            TextView p2losses_textview = (TextView)findViewById(R.id.p2_losses);
+            int p2losses = Integer.parseInt(p2losses_textview.getText().toString());
+            p2losses++;
+            p2losses_textview.setText(p2losses);
+        }
+
+        if(player == 1) //If opponent wins
+        {
+            if(againstDroid)// If user played against droid
+            {
+                TextView cpuwins_textview = (TextView)findViewById(R.id.cpu_wins);
+                int cpuwins = Integer.parseInt(cpuwins_textview.getText().toString());
+                cpuwins++;
+                cpuwins_textview.setText(cpuwins);
+            }
+            else // means user played against a human
+            {
+                //Get textview of player 2's wins and increment
+                TextView p2wins_textview = (TextView)findViewById(R.id.p2_wins);
+                int p2wins = Integer.parseInt(p2wins_textview.getText().toString());
+                p2wins++;
+                p2wins_textview.setText(p2wins);
+            }
+
+
+            //Increment losses of player 1
+            TextView p1losses_textview = (TextView)findViewById(R.id.p1_losses);
+            int p1losses = Integer.parseInt(p1losses_textview.getText().toString());
+            p1losses++;
+            p1losses_textview.setText(p1losses);
+        }
+
         if(player == 9)//Tie game;
-            ;
+        {
+            if(againstDroid)
+            {
+                TextView cputies_textview = (TextView)findViewById(R.id.cpu_ties);
+                int cputies = Integer.parseInt(cputies_textview.getText().toString());
+                cputies++;
+                cputies_textview.setText(cputies);
+            }
+            else
+            {
+                TextView p2ties_textview = (TextView)findViewById(R.id.p2_ties);
+                int p2ties = Integer.parseInt(p2ties_textview.getText().toString());
+                p2ties++;
+                p2ties_textview.setText(p2ties);
+            }
+
+            TextView p1ties_textview = (TextView)findViewById(R.id.p1_ties);
+            int p1ties = Integer.parseInt(p1ties_textview.getText().toString());
+            p1ties++;
+            p1ties_textview.setText(p1ties);
+        }
 
 
         //Show win message
@@ -214,7 +273,7 @@ public class MainActivity extends AppCompatActivity {
         TextView t = (TextView)findViewById(R.id.playmode_text);
 
         if(againstDroid)
-            t.setText(R.string.vsDroid);
+            t.setText(vsDroid);
         else
             t.setText(R.string.vsHuman);
 
